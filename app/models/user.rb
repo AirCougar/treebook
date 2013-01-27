dcf1cf190313178894ea20a06e3b1dd219250a65
Tristin Glunt <slayzer1996@gmail.com>
2013-01-27 16:17:10 -0700

    Added tests for user first name, last name, and profile correctness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :profile_name
 
+  validates :first_name, presence: true
 
- has_many :statuses
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true,
+                           uniqueness: true
+
+has_many :statuses
 
   def full_name
   	first_name + " " + last_name
